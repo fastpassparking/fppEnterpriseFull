42e7b0f4a6d59c5e1dc018ba7bda591fff565262
kyleMera <kmera1@knights.ucf.edu>
2014-11-21 14:01:28 -0500

    Updated lotId

--- fpp/ViewController.m
+++ fpp/ViewController.m
@@ -42,7 +42,7 @@
     resultsMsg.hidden = YES;
     
     // TODO : Search for plate and update info.
-    [ParkingPassHandler getParkingPassByLicensePlate:licensePlate.text withLotId:@"5463f4eedfed56e755d24e25" withCompletionHandler:^(BOOL success, parkingPass* returnedParkingPass) {
+    [ParkingPassHandler getParkingPassByLicensePlate:licensePlate.text withLotId:@"546f878d09c6115461a56e2a" withCompletionHandler:^(BOOL success, parkingPass* returnedParkingPass) {
         
         dispatch_async(dispatch_get_main_queue(), ^{
             expirationMsg.text = @"";
